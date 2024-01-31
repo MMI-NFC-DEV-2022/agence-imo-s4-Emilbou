152983764be3d719baecff952932b0d473e03997
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2024-01-31 14:25:36 +0100

    suppression pocketbase

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import Auth from './components/Auth.vue'
-
-import { onErrorCaptured, ref } from 'vue'
-
-const error = ref<Error | null>(null)
-
-onErrorCaptured((err, component, info) => {
-  console.log({ err, component, info })
-  error.value = err
-  return true
-})
 </script>
 
 <template>
   <header>
     <h1>Test</h1>
-
-    <p v-if="error">Erreur : {{ error.message }}</p>
-
-    <Auth />
 
     <nav class="*:text-red-500 flex flex-col">
       <RouterLink to="/">Home</RouterLink>
@@ -27,13 +12,5 @@ onErrorCaptured((err, component, info) => {
     </nav>
   </header>
 
-  <Suspense>
-    <template #default>
-      <RouterView />
-    </template>
-
-    <template #fallback>
-      <div>Loading...</div>
-    </template>
-  </Suspense>
+  <RouterView />
 </template>
