c529eae934d06a1d57aa9ef82a6a39441560864d
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2024-02-13 08:08:38 +0100

    affichage donnée supabase

---
+++ src/pages/quartier/index.vue
@@ -0,0 +1,27 @@
+
+<script setup lang="ts">
+import { supabase } from "../../supabase";
+import groupBy from "lodash/groupBy";
+
+const { data, error } = await supabase.from("quartiercommune").select("*");
+if (error) console.log("n'a pas pu charger la table quartiercommune :", error);
+</script>
+
+<template>
+    <section class="flex flex-col">
+        <h3 class="text-2xl">Liste des quartiers</h3>
+        <ul>
+            <li v-for="quartierObject in (data as any[])">
+                {{ quartierObject.libelle_Commune }} -
+                {{ quartierObject.libelle_Quartier }}
+            </li>
+        </ul>
+    </section>
+    <Disclosure
+        v-for="(listeQuartier, libelleCommune) in groupBy(
+            data,
+            'libelle_Commune'
+        )"
+        :key="libelleCommune"
+      />
+</template>
