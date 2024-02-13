67c0374fb1579beb56026ed518163f96129ee427
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2024-02-13 16:16:25 +0100

    groupby

--- src/pages/quartier/index.vue
+++ src/pages/quartier/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { supabase } from "../../supabase";
+import groupBy from "lodash/groupBy";
 const { data, error } = await supabase.from("quartier_commune").select("*");
 if (error) console.log("n'a pas pu charger la table quartiercommune :", error);
 </script>
@@ -8,10 +9,19 @@ if (error) console.log("n'a pas pu charger la table quartiercommune :", error);
   <section class="flex flex-col">
     <h3 class="text-2xl">Liste des quartiers</h3>
     <ul>
-      <li v-for="quartierObject in (data as any[])">
-        {{ quartierObject.nomCommune }} -
-        {{ quartierObject.nom_quartier }}
+      <li v-for="(lesquartiers, nomCommune) in (Object.groupBy(data, v => v.nomCommune))">
+<h2>{{ nomCommune }}</h2>
+<li v-for="quartierObject in lesquartiers">
+    <div>{{ quartierObject.nom_quartier }}</div>
+</li>
       </li>
     </ul>
   </section>
+  <Disclosure
+        v-for="(listeQuartier, libelleCommune) in groupBy(
+            data,
+            'nomCommune'
+        )"
+        :key="libelleCommune"
+      />
 </template>
